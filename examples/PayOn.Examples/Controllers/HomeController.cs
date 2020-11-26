a28e1abd3d55e62a73f5ed765f444196ff83fe3c
Andre Hauptfleisch <ahaupt@gmail.com>
2020-11-26 22:45:17 +0200

    feat(.net 5.0 upgrade): extended the payon examples to include repeat recurring payments and reversals

--- examples/PayOn.Examples/Controllers/HomeController.cs
+++ examples/PayOn.Examples/Controllers/HomeController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PayOn.Examples.Models;
 using PayOn.Models;
+using PayOn.Infrastructure.Extensions;
 
-namespace PayOn.Examples.Controllers
+namespace PayOn.Examples.Controllersz
 {
     public class HomeController : Controller
     {
+        private static string CachedRegistrationId;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -58,51 +59,66 @@ namespace PayOn.Examples.Controllers
                     PaymentBrand = model.PaymentBrand
                 },
                 Currency = "ZAR",
-                Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH\\:mm\\:ss.ffzzz", System.Globalization.CultureInfo.InvariantCulture),
-                ShopperResultUrl = $"{Request.Scheme}://{Request.Host}/Home/Confirm"
+                Timestamp = DateTime.Now.ToIso8601String(),
+                ShopperResultUrl = $"{Request.Scheme}://{Request.Host}/Home/ThreeDSecureConfirm"
             };
 
             try
             {
                 ThreeDSecurePaymentResponse response = client.RequestThreeDSecurePayment(request);
 
+                if (response.Result.GetResultStatus() == ResultStatus.Approved)
+                {
+                    model.Status = $"Card not enrolled for 3D Secure. Result code: {response.Result.Code}. Result Description: {response.Result.Description}";
+                    return View(model);
+                }
+
+                if (response.Result.GetResultStatus() == ResultStatus.Declined)
+                {
+                    model.Status = $"Declined. Result code: {response.Result.Code}. Result Description: {response.Result.Description}";
+                    return View(model);
+                }
+
+                if (response.Result.GetResultStatus() != ResultStatus.Pending)
+                {
+                    model.Status = $"Unexpected response. Result code: {response.Result.Code}. Result Description: {response.Result.Description}";
+                    return View(model);
+                }
+
                 return Content(GetRedirectPageHtml(response), "text/html");
             }
             catch (Exception ex)
             {
                 _logger.LogError("Unable to start 3D Secure Transaction", ex);
-                model.Status = "Unable to start 3D Secure Transaction. Please see logs.";
+                model.Status = $"Unable to start 3D Secure Transaction. {ex}";
             }
-            
+
             return View(model);
         }
 
-        private string GetRedirectPageHtml(ThreeDSecurePaymentResponse threeDSecurePaymentResponse)
-        {
-            Dictionary<string, string> dict = threeDSecurePaymentResponse.Redirect.Parameter
-               .ToDictionary(x => x.Name, x => x.Value);
-
-            HttpClient client = new HttpClient();
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, threeDSecurePaymentResponse.Redirect.Url)
-            {
-                Content = new FormUrlEncodedContent(dict)
-            };
-            HttpResponseMessage response = client.SendAsync(request).Result;
-            return response.Content.ReadAsStringAsync().Result;
-        }
-
-        public IActionResult Confirm(string id, string resourcePath)
+        public IActionResult ThreeDSecureConfirm(string id, string resourcePath)
         {
             PayOnClient client = new PayOnClient(Settings.EntityIdThreeDSecure, Settings.UserId, Settings.Password, Settings.BaseUrl);
-            ThreeDSecurePaymentStatusResponse paymentStatus = client.RequestThreeDSecurePaymentStatus(id);
             PaymentViewModel model = new PaymentViewModel
             {
-                Status = Enum.GetName(typeof(ResultStatus), paymentStatus.Result.GetResultStatus())
+                Id = id
             };
+
+            try
+            {
+                ThreeDSecurePaymentStatusResponse paymentStatus = client.RequestThreeDSecurePaymentStatus(id);
+                model.Status = $"{Enum.GetName(typeof(ResultStatus), paymentStatus.Result.GetResultStatus())}. Result code: {paymentStatus.Result.Code}. Result Description: {paymentStatus.Result.Description}"; ;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to start 3D Secure Transaction", ex);
+                model.Status = $"Unable to start 3D Secure Transaction. {ex}";
+            }
 
             return View(model);
         }
-        public IActionResult Recurring()
+
+        public IActionResult RecurringInitial()
         {
             DateTime FutureDate = DateTime.Now.AddYears(1);
 
@@ -116,60 +132,187 @@ namespace PayOn.Examples.Controllers
                 CardExpiryMonth = FutureDate.ToString("MM"),
                 CardExpiryYear = FutureDate.ToString("yyyy"),
                 CardCvv = "123",
-                RegistrationId = "Submit to register card",
-                Status = "N/A"
+                RegistrationId = "Submit to register card"
             };
 
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult Recurring(PaymentViewModel model)
+        public IActionResult RecurringInitial(PaymentViewModel model)
         {
-            // TODO: Enable this again
-            // PayOnClient client = new PayOnClient(Settings.EntityId_Recurring, Settings.UserId, Settings.Password, Settings.BaseUrl);
-            // PaymentRequest request = new PaymentRequest
-            // {
-            //     PaymentBrand = model.PaymentBrand,
-            //     Amount = model.Amount,
-            //     Card = new CardAccount
-            //     {
-            //         Holder = model.CardHolder,
-            //         Number = model.CardNumber,
-            //         ExpiryMonth = model.CardExpiryMonth,
-            //         ExpiryYear = model.CardExpiryYear,
-            //         Cvv = model.CardCvv,
-            //         ThreeDSecure = true,
-            //         PaymentBrand = model.PaymentBrand
-            //     },
-            //     Currency = "ZAR",
-            //     Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH\\:mm\\:ss.ffzzz", System.Globalization.CultureInfo.InvariantCulture)
-            // };
+            PayOnClient client = new PayOnClient(Settings.EntityIdThreeDSecure, Settings.UserId, Settings.Password, Settings.BaseUrl);
+            PaymentRequest request = new PaymentRequest
+            {
+                PaymentBrand = model.PaymentBrand,
+                Amount = model.Amount,
+                Card = new CardAccount
+                {
+                    Holder = model.CardHolder,
+                    Number = model.CardNumber,
+                    ExpiryMonth = model.CardExpiryMonth,
+                    ExpiryYear = model.CardExpiryYear,
+                    Cvv = model.CardCvv,
+                    PaymentBrand = model.PaymentBrand
+                },
+                Currency = "ZAR",
+                PaymentType = "DB",
+                RecurringType = "INITIAL",
+                CreateRegistration = true,
+                Timestamp = DateTime.Now.ToIso8601String(),
+                ShopperResultUrl = $"{Request.Scheme}://{Request.Host}/Home/RecurringInitialConfirm"
+            };
 
-            // PaymentResponse response = null;
+            try
+            {
+                PaymentResponse response = client.RequestPayment(request);
 
-            // // If first submission register card first
-            // if (model.RegistrationId.Length != 32)
-            // {
-            //     response = client.RequestInitialRecurringPayment(request);
-            //     model.RegistrationId = response.RegistrationId;
-            //     model.Status = "Initial Registration and Payment - " + Enum.GetName(typeof(ResultStatus), response.Result.GetResultStatus());
-            // }
-            // else
-            // {
-            //     response = client.RequestRepeatRecurringPayment(request, model.RegistrationId);
-            //     model.Status = "Repeat Recurring Payment - " + Enum.GetName(typeof(ResultStatus), response.Result.GetResultStatus());
-            // }
+                model.Id = response.Id;
 
-            // ModelState.Clear();
+                // Not enrolled for 3D Secure
+                if (response.Result.GetResultStatus() == ResultStatus.Approved)
+                {
+                    return RedirectToAction("RedirectComplete", response.RegistrationId);
+                }
+
+                if (response.Result.GetResultStatus() == ResultStatus.Declined || response.Result.GetResultStatus() != ResultStatus.Pending)
+                {
+                    model.Status = $"{Enum.GetName(typeof(ResultStatus), response.Result.GetResultStatus())}. Result code: {response.Result.Code}. Result Description: {response.Result.Description}";
+                    return View(model);
+                }
+
+                CachedRegistrationId = response.RegistrationId;
+
+                return Content(GetRedirectPageHtml(response), "text/html");
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to start Recurring Transaction", ex);
+                model.Status = $"Unable to start Recurring Transaction. {ex}";
+            }
 
             return View(model);
+        }
+
+        public IActionResult RecurringInitialConfirm(string id, string resourcePath)
+        {
+            PayOnClient client = new PayOnClient(Settings.EntityIdThreeDSecure, Settings.UserId, Settings.Password, Settings.BaseUrl);
+            PaymentViewModel model = new PaymentViewModel
+            {
+                Id = id
+            };
+
+            try
+            {
+                PaymentStatusResponse paymentStatus = client.RequestPaymentStatus(id);
+                model.Status = $"{Enum.GetName(typeof(ResultStatus), paymentStatus.Result.GetResultStatus())}. Result code: {paymentStatus.Result.Code}. Result Description: {paymentStatus.Result.Description}";
+                model.RegistrationId = CachedRegistrationId;
+                model.Amount = "234.56";
+                model.Currency = "ZAR";
+                model.PaymentBrand = paymentStatus.PaymentBrand;
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to start 3D Secure Transaction", ex);
+                model.Status = $"Unable to start 3D Secure Transaction. {ex}";
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult RecurringRepeat(PaymentViewModel model)
+        {
+            PayOnClient client = new PayOnClient(Settings.EntityIdRecurring, Settings.UserId, Settings.Password, Settings.BaseUrl);
+            RegistrationRequest recurringRequest = new RegistrationRequest
+            {
+                PaymentBrand = model.PaymentBrand,
+                Amount = model.Amount,
+                Currency = model.Currency,
+                RecurringType = "REPEATED",
+                PaymentType = "DB",
+                CreateRegistration = false,
+                RegistrationId = model.RegistrationId
+            };
+
+            try
+            {
+                RegistrationResponse response = client.RequestRegistration(recurringRequest);
+                model.Id = response.Id;
+                model.Status = $"{Enum.GetName(typeof(ResultStatus), response.Result.GetResultStatus())}. Result code: {response.Result.Code}. Result Description: {response.Result.Description}";
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to start 3D Secure Transaction", ex);
+                model.Status = $"Unable to start 3D Secure Transaction. {ex}";
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Reverse(string id)
+        {
+            PayOnClient client = new PayOnClient(Settings.EntityIdThreeDSecure, Settings.UserId, Settings.Password, Settings.BaseUrl);
+            PaymentViewModel model = new PaymentViewModel();
+
+            try
+            {
+                PaymentRequest paymentRequest = new PaymentRequest
+                {
+                    PaymentType = "RV",
+                };
+                PaymentResponse response = client.RequestPaymentReversal(id);
+
+                model.Status = $"{Enum.GetName(typeof(ResultStatus), response.Result.GetResultStatus())}. Result code: {response.Result.Code}. Result Description: {response.Result.Description}";
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unable to start 3D Secure Transaction", ex);
+                model.Status = $"Unable to start 3D Secure Transaction. {ex}";
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Reverse(PaymentViewModel model)
+        {
+            return Reverse(model.Id);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string GetRedirectPageHtml(PaymentResponse response)
+        {
+            return GetRedirectPageHtml(response.Redirect);
+        }
+
+        private string GetRedirectPageHtml(ThreeDSecurePaymentResponse response)
+        {
+            return GetRedirectPageHtml(response.Redirect);
+        }
+
+        private string GetRedirectPageHtml(Redirect redirect)
+        {
+            Dictionary<string, string> dict = redirect.Parameter
+               .ToDictionary(x => x.Name, x => x.Value);
+
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, redirect.Url)
+            {
+                Content = new FormUrlEncodedContent(dict)
+            };
+            HttpResponseMessage response = client.SendAsync(request).Result;
+            return response.Content.ReadAsStringAsync().Result;
         }
     }
 }
--- examples/PayOn.Examples/Models/PaymentViewModel.cs
+++ examples/PayOn.Examples/Models/PaymentViewModel.cs
@@ -3,6 +3,7 @@ namespace PayOn.Examples.Models
 {
     public class PaymentViewModel
     {
+        public string Id { get; set; }
         public string Amount { get; set; }
         public string Currency { get; set; }
         public string PaymentBrand { get; set; }
